731f7ba6d1a4ed580794d1b15329f322b98090f8
ryoshimaru <karton.eu@yandex.ru>
2026-05-20 23:43:27 +0700

    add models

---
+++ internal/models/models.go
@@ -0,0 +1,19 @@
+package models
+
+import "time"
+
+type Department struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	ParentID  *int      `json:"parent_id"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Employee struct {
+	ID           int        `json:"id"`
+	DepartmentID int        `json:"department_id"`
+	FullName     string     `json:"full_name"`
+	Position     string     `json:"position"`
+	HiredAt      *time.Time `json:"hired_at"`
+	CreatedAt    time.Time  `json:"created_at"`
+}
