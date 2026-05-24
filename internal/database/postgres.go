ae86e0b1fc973a02bc98cc19de42da2914815102
ryoshimaru <karton.eu@yandex.ru>
2026-05-24 17:42:48 +0700

    add layer for working with memory and config for connection

---
+++ internal/database/postgres.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/ryoshimaru/hittalent/internal/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect(cfg config.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBName,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
